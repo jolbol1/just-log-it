4f04f2706b8f48eabffca963c39cf30a8862b0a4
jolbol1 <github@jamesshopland.com>
2023-08-19 21:36:28 +0100

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,66 @@
+export default function AboutPage() {
+  return (
+    <main className="w-full p-4 md:p-10 mx-auto max-w-7xl">
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 ">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-12 ">
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl text-center">
+            About Us
+          </h1>
+          <p className="leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Empower Your Weight Journey with JustLogIt, we believe in the power
+            of simplicity. The road to a healthier you should be straightforward
+            and clutter-free, and that&apos;s precisely what we offer—a weight
+            tracking platform that&apos;s as easy to use as it is effective.
+          </p>
+          <h2 className="font-heading text-xl sm:text-2xl md:text-3xl  text-center">
+            🚀 Fast and Efficient
+          </h2>
+          <p className="leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            In our digital age, we&apos;re surrounded by countless apps and
+            platforms, many of which come with a steep learning curve. At
+            JustLogIt, we&apos;re going against the grain. Our platform is built
+            on the principle of intuitive design. From the moment you sign up,
+            you&apos;ll experience the seamless nature of our interface. Whether
+            you&apos;re logging today&apos;s weight or glancing at past records,
+            every feature is just a click away. Our emphasis on a frictionless
+            experience means less time figuring things out and more time
+            focusing on what truly matters: your progress.
+          </p>
+          <h2 className="font-heading text-xl sm:text-2xl md:text-3xl  text-center">
+            👁️ Clear Visual Insights
+          </h2>
+          <p className="leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            A successful journey is built on understanding. With our platform,
+            you&apos;re not just inputting data; you&apos;re gaining insight.
+            Our state-of-the-art visual representation tools transform raw
+            numbers into comprehensible charts and graphs. With each log, watch
+            as your journey unfolds before your eyes, offering both motivation
+            and a clear picture of where you stand. Every stride forward, every
+            minor setback—it&apos;s all there, painted in vivid detail, ensuring
+            you&apos;re never in the dark about your progress.
+          </p>
+          <h2 className="font-heading text-xl sm:text-2xl md:text-3xl  text-center">
+            🏆 Goals Tailored for You
+          </h2>
+          <p className="leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            We celebrate individuality. We understand that every person&apos;s
+            journey is unique, filled with personal challenges and aspirations.
+            Our adaptive goal-setting feature is designed to recognize this
+            individuality. Instead of one-size-fits-all targets, JustLogIt
+            crafts milestones that resonate with your personal journey. These
+            targets strike the perfect balance—pushing you to achieve more, yet
+            always staying within reach. Your health journey deserves a
+            dedicated partner, and we&apos;re here to fill that role. At
+            JustLogIt, every feature, every tool, every line of code is crafted
+            with your success in mind. We&apos;re not just a platform;
+            we&apos;re a community committed to seeing our members thrive. Join
+            us in redefining weight tracking. Embrace a world where technology
+            meets personal care, where data meets understanding, and where every
+            day is a step closer to your goals. Choose JustLogIt. Let&apos;s
+            journey together towards a healthier, brighter future.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
