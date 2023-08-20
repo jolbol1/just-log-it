8a04e6314e522551fa3f866408e402eebd0dd0fe
jolbol1 <github@jamesshopland.com>
2023-08-20 22:27:56 +0100

    add greying out

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -49,57 +49,64 @@ export function MobileNav({ children, user }: MobileNavProps) {
           <span className="font-bold">Menu</span>
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-[100vw] mt-3 rounded-none border-x-0 ">
-        {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <div
+        className={cn({
+          'after:top-14 after:bg-background/95 after:absolute  after:right-0 after:left-0 after:bottom-0 after:z-[-1] after:h-[calc(100vh-3.5rem)] after:w-screen':
+            open
+        })}
+      >
+        <DropdownMenuContent className="w-[100vw] mt-3 rounded-none border-x-0 ">
+          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator /> */}
-        <DropdownMenuGroup>
-          <DropdownMenuItem asChild className="h-12">
-            <Link href="/">
-              <span
-                className={cn(
-                  'ml-6 font-medium text-foreground/80',
-                  pathname === '/' ? 'text-foreground' : 'text-foreground/60'
-                )}
-              >
-                Home
-              </span>
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild className="h-12">
-            <Link href="/about">
-              <span
-                className={cn(
-                  'ml-6 font-medium text-foreground/80',
-                  pathname === '/about'
-                    ? 'text-foreground'
-                    : 'text-foreground/60'
-                )}
-              >
-                About
-              </span>
-            </Link>
-          </DropdownMenuItem>
-          {user && (
-            <>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem asChild className="h-12">
-                <Link href="/dashboard">
-                  <span
-                    className={cn(
-                      'ml-6 font-medium text-foreground/80',
-                      pathname === '/dashboard'
-                        ? 'text-foreground'
-                        : 'text-foreground/60'
-                    )}
-                  >
-                    Dashboard
-                  </span>
-                </Link>
-              </DropdownMenuItem>
-            </>
-          )}
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuItem asChild className="h-12">
+              <Link href="/">
+                <span
+                  className={cn(
+                    'ml-6 font-medium text-foreground/80',
+                    pathname === '/' ? 'text-foreground' : 'text-foreground/60'
+                  )}
+                >
+                  Home
+                </span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild className="h-12">
+              <Link href="/about">
+                <span
+                  className={cn(
+                    'ml-6 font-medium text-foreground/80',
+                    pathname === '/about'
+                      ? 'text-foreground'
+                      : 'text-foreground/60'
+                  )}
+                >
+                  About
+                </span>
+              </Link>
+            </DropdownMenuItem>
+            {user && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild className="h-12">
+                  <Link href="/dashboard">
+                    <span
+                      className={cn(
+                        'ml-6 font-medium text-foreground/80',
+                        pathname === '/dashboard'
+                          ? 'text-foreground'
+                          : 'text-foreground/60'
+                      )}
+                    >
+                      Dashboard
+                    </span>
+                  </Link>
+                </DropdownMenuItem>
+              </>
+            )}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </div>
     </DropdownMenu>
   );
 }
